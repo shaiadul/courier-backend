3c57ec54a0c3ce6f30dceaf3832b71b233aa87c9
shaiadul <devshaiadul69@gmail.com>
2025-06-27 12:06:36 +0600

    cors: policy add

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
