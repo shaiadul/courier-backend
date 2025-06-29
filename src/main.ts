a8fc8a051bf2684ae3f09f5d0081e4c55bcc974a
shaiadul <devshaiadul69@gmail.com>
2025-06-29 15:40:00 +0600

    origin: set new vercel origin

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://courier-frontend-iota.vercel.app',
+    ],
     credentials: true,
   });
   app.useGlobalFilters(new AllExceptionsFilter());
